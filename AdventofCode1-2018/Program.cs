0c70a9da932731a517ba98871c3f9a5b6fea5445
Michael Scott <michael.r.scott97@gmail.com>
2019-09-11 19:37:56 -0400

    Part 1 Complete

--- AdventofCode1-2018/Program.cs
+++ AdventofCode1-2018/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AdventofCode1_2018
 {
@@ -10,6 +11,49 @@ namespace AdventofCode1_2018
     {
         static void Main(string[] args)
         {
+            Console.ReadKey();
+            GrabValues();
+            Console.ReadKey();
+        }
+
+        private static void GrabValues()
+        {
+
+            string line;
+            int tempVal = 0;
+            int total = 0;
+            StreamReader streamReader = new StreamReader(@"C:\Users\David\Desktop\values.txt");
+            line = streamReader.ReadLine();
+            while (line != null)
+            {
+
+                string giveTake = String.Empty;
+                giveTake = line;
+                Console.WriteLine(giveTake);
+
+                tempVal = Convert.ToInt32(giveTake.Substring(1));
+
+                char plusMinus = giveTake[0];
+
+                if (plusMinus.Equals('+'))
+                {
+                    total += tempVal;
+                }
+                else if (plusMinus.Equals('-'))
+                {
+                    total -= tempVal;
+                }
+                else
+                {
+                    Console.WriteLine("problem");
+                }
+
+                Console.WriteLine(total);
+                Console.WriteLine();
+
+                line = streamReader.ReadLine();
+            }
+            streamReader.Close();
         }
     }
 }
